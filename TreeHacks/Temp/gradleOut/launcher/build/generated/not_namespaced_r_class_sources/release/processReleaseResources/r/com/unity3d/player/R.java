b768f65fd6676d924b0adb4efa8b7335668f0526
TheBestestGuestest <ryanniu314@gmail.com>
2020-02-15 01:49:39 -0800

    better ar

--- TreeHacks/Temp/gradleOut/launcher/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/com/unity3d/player/R.java
+++ TreeHacks/Temp/gradleOut/launcher/build/generated/not_namespaced_r_class_sources/release/processReleaseResources/r/com/unity3d/player/R.java
@@ -9,11 +9,37 @@ package com.unity3d.player;
 public final class R {
     private R() {}
 
+    public static final class id {
+        private id() {}
+
+        public static final int __arcore_cancelButton = 0x7f010000;
+        public static final int __arcore_continueButton = 0x7f010001;
+        public static final int __arcore_messageText = 0x7f010002;
+    }
+    public static final class layout {
+        private layout() {}
+
+        public static final int __arcore_education = 0x7f020000;
+    }
+    public static final class raw {
+        private raw() {}
+
+        public static final int keep_arcore = 0x7f040000;
+    }
+    public static final class string {
+        private string() {}
+
+        public static final int __arcore_cancel = 0x7f050000;
+        public static final int __arcore_continue = 0x7f050001;
+        public static final int __arcore_install_app = 0x7f050002;
+        public static final int __arcore_install_feature = 0x7f050003;
+        public static final int __arcore_installing = 0x7f050004;
+    }
     public static final class style {
         private style() {}
 
-        public static final int BaseUnityTheme = 0x7f030000;
-        public static final int UnityThemeSelector = 0x7f030001;
-        public static final int UnityThemeSelector_Translucent = 0x7f030002;
+        public static final int BaseUnityTheme = 0x7f060000;
+        public static final int UnityThemeSelector = 0x7f060001;
+        public static final int UnityThemeSelector_Translucent = 0x7f060002;
     }
 }
