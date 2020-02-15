bd03322b6953fef5b1b9de17e2b8007435cfc149
Connor Lien <connorlien19@gmail.com>
2020-02-15 12:05:20 -0800

    Updated App.cs
    but it still doesnt work idk why

--- TreeHacks/Assets/Scripts/App.cs
+++ TreeHacks/Assets/Scripts/App.cs
@@ -7,13 +7,15 @@ public class App : MonoBehaviour
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     private static void OnAppStart()
     {
+        Debug.Log("Firebase Test started");
         var testmessage = new Message(new Vector3(0, 0, 0), "Connor", "Hello World!");
         ServerManager.PostMessage(testmessage, "12", () => 
+        { 
+            Debug.Log("Post successful"); 
+        });
+        ServerManager.GetMessage("12", info =>
         {
-            ServerManager.GetMessage("11", info =>
-            {
-                Debug.Log($"{info.location}{info.username}{info.message}");
-            });
+            Debug.Log($"{info.location}{info.username}{info.message}");
         });
     }
 }
