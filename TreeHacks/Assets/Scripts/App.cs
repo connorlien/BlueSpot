668aa173052c7e57b7f8169bcbcc9398baef768b
Connor Lien <connorlien19@gmail.com>
2020-02-15 11:51:51 -0800

    created App.cs

---
+++ TreeHacks/Assets/Scripts/App.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class App : MonoBehaviour
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+    private static void OnAppStart()
+    {
+        var testmessage = new Message(new Vector3(0, 0, 0), "Connor", "Hello World!");
+        ServerManager.PostMessage(testmessage, "12", () => 
+        {
+            ServerManager.GetMessage("11", info =>
+            {
+                Debug.Log($"{info.location}{info.username}{info.message}");
+            });
+        });
+    }
+}
